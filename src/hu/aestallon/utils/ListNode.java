058c50dd6c639f9c536cd20356d6b5a0867613bd
Papp Szabolcs <papp.szabolcs.bazil@gmail.com>
2022-03-27 15:35:39 +0200

    [Main-003c] Updated ListNode documentation

--- src/hu/aestallon/utils/ListNode.java
+++ src/hu/aestallon/utils/ListNode.java
@@ -1,17 +1,23 @@
 package hu.aestallon.utils;
 
 /**
- * Provided for <b>2. Add Two Numbers</b>
+ * Describes a linked list for storing numbers
+ * in base10 using "big endian" notation.
+ * <p>
+ * Provided for <b>2. Add Two Numbers</b>.
+ * </p>
  */
 public class ListNode {
     public int val;
     public ListNode next;
 
-    public ListNode() {}
+    public ListNode() {
+    }
 
     public ListNode(int val) {
         this.val = val;
     }
+
     public ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
